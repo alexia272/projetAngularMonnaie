0425cd18f9a86dd38b0d355cfa9406fd474a7a5c
BenoitDarmigny <66302127+BenoitDarmigny@users.noreply.github.com>
2020-11-17 15:39:26 +0100

    I am the boss bitch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { MoneyConvertorComponent } from './money-convertor/money-convertor.component';
 
 const routes: Routes = [
   {
     path: '', component: HomeComponent
+
+  },
+  {
+    path: 'MoneyConvertor', component: MoneyConvertorComponent
   }
 ];
 
