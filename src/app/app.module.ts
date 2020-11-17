6f2cbcac15f3a3ec452e8bc5ea009a26a7b95358
alexia272 <alexia.berger@isen.yncrea.fr>
2020-11-17 15:17:28 +0100

    benoit t chiant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 import { AppComponent } from './app.component';
 import { MoneyConvertorComponent } from './money-convertor/money-convertor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MoneyConvertorComponent
+    MoneyConvertorComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
