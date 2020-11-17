f11b78fde70f1dbd50d8fbd19fb8454d71b9bba2
BenoitDarmigny <66302127+BenoitDarmigny@users.noreply.github.com>
2020-11-17 14:55:48 +0100

    création money converter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MoneyConvertorComponent } from './money-convertor/money-convertor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoneyConvertorComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/money-convertor/money-convertor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-money-convertor',
+  templateUrl: './money-convertor.component.html',
+  styleUrls: ['./money-convertor.component.css']
+})
+export class MoneyConvertorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
